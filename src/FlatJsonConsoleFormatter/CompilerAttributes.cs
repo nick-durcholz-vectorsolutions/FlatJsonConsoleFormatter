fb2c6dd54e28a708de89e4838c49597556b4b26b
Deierlein Felix BIT <felix.deierlein@bit.admin.ch>
2024-04-24 19:46:43 +0200

    chore: Reformat and cleanup ...

--- src/FlatJsonConsoleFormatter/CompilerAttributes.cs
+++ src/FlatJsonConsoleFormatter/CompilerAttributes.cs
@@ -8,13 +8,19 @@ internal sealed class MemberNotNullAttribute : Attribute
     /// <param name="member">
     ///     The field or property member that is promised to be not-null.
     /// </param>
-    public MemberNotNullAttribute(string member) => Members = new[] { member };
+    public MemberNotNullAttribute(string member)
+    {
+        Members = new[] { member };
+    }
 
     /// <summary>Initializes the attribute with the list of field and property members.</summary>
     /// <param name="members">
     ///     The list of field and property members that are promised to be not-null.
     /// </param>
-    public MemberNotNullAttribute(params string[] members) => Members = members;
+    public MemberNotNullAttribute(params string[] members)
+    {
+        Members = members;
+    }
 
     /// <summary>Gets field or property member names.</summary>
     public string[] Members { get; }
@@ -22,4 +28,6 @@ internal sealed class MemberNotNullAttribute : Attribute
 
 /// <summary>Specifies that a method that will never return under any circumstance.</summary>
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-internal sealed class DoesNotReturnAttribute : Attribute { }
+internal sealed class DoesNotReturnAttribute : Attribute
+{
+}
